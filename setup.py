5f8c3aee34ffdc9c7639411fd7b1c5852b60e17c
Adam Jermyn <adamjermyn@gmail.com>
2017-07-12 12:20:08 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='arrfunc',
+      version='1.0',
+      description='Lazy evaluation to treat functions as numpy arrays.',
+      url='http://github.com/adamjermyn/arrfunc',
+      author='Adam S. Jermyn',
+      author_email='adamjermyn@example.com',
+      license='MIT',
+      packages=['arrfunc'],
+      zip_safe=False)
